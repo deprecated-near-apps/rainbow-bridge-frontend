9dfcc1bbcb95f6100c8af2b1579be2aaac1cdd96
Mario Halambek <mario33halambek@gmail.com>
2021-02-19 18:28:07 +0000

    Sentry monitoring
    Add sentry SDK for monitoring errors.

---
+++ src/js/sentry.js
@@ -0,0 +1,8 @@
+import * as Sentry from '@sentry/browser'
+import { Integrations } from '@sentry/tracing'
+
+Sentry.init({
+  dsn: 'https://aa1b5a908e7e438c97400adeac4768bd@o398573.ingest.sentry.io/5644743',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0
+})
